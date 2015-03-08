acc9ba7f9949bd445c0fbd2309ab5eb75dfa4244
Alchan Kim <a9413miky@gmail.com>
2015-03-08 17:37:59 +0900

    Now automated to precompile lemon to make ast-make.c and ast-make.h

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 
+from os.path import join as path_join
+from subprocess import check_call
 from distutils.core import setup
 from distutils.extension import Extension
+from distutils.command.build_ext import build_ext
+from distutils.ccompiler import new_compiler, CCompiler
+
+
+class LemonGen(build_ext):
+    def run(self):
+        cc = new_compiler()
+        cc.link(CCompiler.EXECUTABLE, ["clisn/lemon.c"], "clisn/lemon")
+        check_call([path_join("clisn", "lemon"),
+                    path_join("clisn", "ast-make.y")])
+        build_ext.run(self)
+
+
+
 
 setup(name="LISN",
       version="0.1",
@@ -13,5 +29,6 @@ setup(name="LISN",
                              sources=["clisn/ast.c", "clisn/lexer.c",
                                       "clisn/ast-make.c", "clisn/parser.c", 
                                       "clisn/astmisc.c",
-                                      "clisnmod.c"])])
+                                      "clisnmod.c"])],
+      cmdclass = {"build_ext": LemonGen})
 
